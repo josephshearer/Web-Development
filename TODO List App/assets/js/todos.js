09c2c09d6933e3b31f80f31734faaa84994246ec
Joseph Shearer <joseph.shearer55@gmail.com>
2017-06-28 20:24:20 -0400

    Added functionality to remove items.

--- TODO List App/assets/js/todos.js
+++ TODO List App/assets/js/todos.js
@@ -2,3 +2,10 @@
 $("li").click(function(){
   $(this).toggleClass("completed");
 });
+
+$("ul li span").click(function(event){
+  event.stopPropagation();
+  $(this).parent().fadeOut(500, function(){
+    $(this).remove();
+  });
+});
