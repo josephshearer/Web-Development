c14f771ed212fbf52d4f5fc08cc9d34fc47a4ff3
Joseph Shearer <joseph.shearer55@gmail.com>
2017-06-28 20:38:00 -0400

    added functionality to add new items and remove the new items

--- TODO List App/assets/js/todos.js
+++ TODO List App/assets/js/todos.js
@@ -1,11 +1,19 @@
 //Check off specific todos by clicking
-$("li").click(function(){
+$("ul").on("click", "li", function(){
   $(this).toggleClass("completed");
 });
 
-$("ul li span").click(function(event){
+$("ul").on("click", "span", function(event){
   event.stopPropagation();
   $(this).parent().fadeOut(500, function(){
     $(this).remove();
   });
+});
+
+$("input[type ='text']").keypress(function(event){
+  if(event.which === 13){
+    var todoText = $(this).val();
+    $("ul").append("<li><span>X</span> " + todoText + "</li>");
+    $(this).val("");
+  }
 });
